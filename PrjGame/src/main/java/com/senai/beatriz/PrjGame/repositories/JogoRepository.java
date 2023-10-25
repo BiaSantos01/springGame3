e599cef513453837c6540a570d074f07d440dcf5
biia_santosss_ <133895171+BiaSantos01@users.noreply.github.com>
2023-10-25 09:53:27 -0300

    Add files via upload

---
+++ PrjGame/src/main/java/com/senai/beatriz/PrjGame/repositories/JogoRepository.java
@@ -0,0 +1,12 @@
+package com.senai.beatriz.PrjGame.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.beatriz.PrjGame.entities.Jogo;
+
+public interface JogoRepository extends JpaRepository<Jogo, Long>{
+
+	
+	
+
+}
